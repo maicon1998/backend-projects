43bd00115f17a2155c5437f8f8916f0ee1eaa511
Maicon <maicon.rodrigues017@proton.me>
2025-05-06 16:40:34 -0300

    modified error_messages

--- task-cli/utils/error_messages.py
+++ task-cli/utils/error_messages.py
@@ -1,5 +1,48 @@
-class Msg:
-    missing_description = "Missing description"
-    exceeded = "You exceeded the number of arguments"
-    invalid = "Invalid arguments. Type 'task-cli help' too see the manual"
-    missing_id = "Missing id"
+import sys
+
+
+def valid_args(len_valid_args, error_msg):
+    if len(sys.argv) == len_valid_args:
+        pass
+
+    else:
+        match error_msg:
+            case "delete":
+                return sys.exit("Invalid arguments. Usage: task-cli delete <id>")
+
+            case "mark":
+                return sys.exit(
+                    "Invalid arguments. Usage: task-cli <mark-in-progress> <id> or <mark-done> <id>"
+                )
+
+            case "list":
+                return sys.exit("Invalid arguments. Usage: task-cli list")
+
+            case "list-status":
+                return sys.exit(
+                    "Invalid arguments. Usage: task-cli list <done> or <todo> or <in-progress>"
+                )
+
+
+def min_args(len_min_args, error_msg):
+    if len(sys.argv) != len_min_args:
+        match error_msg:
+            case "add":
+                return sys.exit(
+                    "Invalid arguments. Usage: task-cli add <'description'>"
+                )
+            case "update":
+                return sys.exit(
+                    "Invalid arguments. Usage: task-cli update <id> <description>"
+                )
+
+
+def valid_status(status):
+    if status not in ["done", "todo", "in-progress"]:
+        return sys.exit(
+            "Invalid arguments. Usage: task-cli list <done> or <todo> or <in-progress>"
+        )
+
+
+def check_id(id, data):
+    return next((item for item in data if item["id"] == id), None)
