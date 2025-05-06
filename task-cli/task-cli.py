ce147f264ec4f8273afc304d8fa9ab8e3c0e1821
Maicon <maicon.rodrigues017@proton.me>
2025-05-06 16:35:45 -0300

    fixed excessive uses of if else

--- task-cli/task-cli.py
+++ task-cli/task-cli.py
@@ -4,116 +4,96 @@ import datetime
 from utils.manual import Manual
 from utils.model import Task
 from utils.json import Json_file
-from utils.error_messages import Msg
+from utils.error_messages import valid_args, min_args, valid_status, check_id
 
 from prettytable import PrettyTable
 
 # datetime format
-datetime = datetime.datetime.now().strftime(
-    f"{'%Y'}-{'%m'}-{'%d'} {'%H'}:{'%M'}:{'%S'}"
-)
+current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # load json file
 data = Json_file().load()
 
-# manual
-if sys.argv[1] == "help":
-    print(Manual().manual)
+match sys.argv[1]:
+    # manual
+    case "help":
+        print(Manual().manual)
 
-# new task
-elif sys.argv[1] == "add":
-    if len(sys.argv) == 3:
+    # new task
+    case "add":
+        min_args(3, "add")
+
         # id auto increment
         if not data["tasks"]:
             id = 0
         else:
             id = data["tasks"][-1]["id"] + 1
 
-        new_task = Task(id, sys.argv[2], "todo", datetime, datetime)
+        new_task = Task(id, sys.argv[2], "todo", current_datetime, current_datetime)
         data["tasks"].append(new_task.dictionary())
         Json_file().dump(data, "Added task")
 
-    # missing task description
-    elif len(sys.argv) == 2:
-        sys.exit(Msg.missing_description)
+    # updating tasks
+    case "update":
+        min_args(4, "update")
 
-    # more than 3 argv
-    else:
-        sys.exit(Msg.exceeded)
+        task = check_id(int(sys.argv[2]), data["tasks"])
 
-# updating tasks
-elif sys.argv[1] == "update":
-    if len(sys.argv) == 4:
-        for item in data["tasks"]:
-            if item["id"] == int(sys.argv[2]):
-                item["description"] = sys.argv[3]
-                item["updatedAt"] = datetime
-                break
+        if task:
+            task["description"] = sys.argv[3]
+            task["updatedAt"] = current_datetime
+            Json_file().dump(data, "Updated task")
+        else:
+            sys.exit("No task with this id")
 
-        Json_file().dump(data, "Updated task")
+    # deleting tasks
+    case "delete":
+        valid_args(3, "delete")
 
-    # missing task id
-    elif len(sys.argv) == 2:
-        sys.exit(Msg.missing_id)
+        check_id = check_id(int(sys.argv[2]), data["tasks"])
 
-    # missing task description
-    elif len(sys.argv) == 3:
-        sys.exit("Msg.missing_description")
+        if check_id:
+            data["tasks"] = [
+                item for item in data["tasks"] if item["id"] != int(sys.argv[2])
+            ]
+            Json_file().dump(data, "Deleted task")
+        else:
+            sys.exit("No task with this id")
 
-    # more than 4 args
-    else:
-        sys.exit(Msg.exceeded)
+    # listing all tasks
+    case "list":
+        if not data["tasks"]:
+            sys.exit("No tasks")
 
-# deleting tasks
-elif sys.argv[1] == "delete":
-    if len(sys.argv) == 3:
-        data["tasks"] = [
-            item for item in data["tasks"] if item["id"] != int(sys.argv[2])
-        ]
-        Json_file().dump(data, "Deleted task")
+        table = PrettyTable(data["tasks"][0].keys())
 
-    # missing task id
-    elif len(sys.argv) == 2:
-        sys.exit(Msg.missing_id)
+        if len(sys.argv) < 3:
+            for item in data["tasks"]:
+                table.add_row(item.values())
 
-    # more than 3 argv
-    else:
-        sys.exit(Msg.exceeded)
+        else:
+            # list tasks by status
+            valid_args(3, "list-status")
+            valid_status(sys.argv[2])
+            for item in [
+                item for item in data["tasks"] if item["status"] == sys.argv[2]
+            ]:
+                table.add_row(item.values())
 
-# listing all tasks
-elif sys.argv[1] == "list":
-    table = PrettyTable(data["tasks"][0].keys())
-
-    if len(sys.argv) < 3:
-        for item in data["tasks"]:
-            table.add_row(item.values())
         print(table)
 
-    # invalid status
-    elif sys.argv[2] not in ["done", "todo", "in-progress"]:
-        sys.exit("Invalid argument. Options: done, todo, in-progress")
+    # uptated the status
+    case "mark-in-progress" | "mark-done":
+        valid_args(3, "mark")
 
-    # list tasks by status
-    else:
-        for item in data["tasks"]:
-            if item["status"] == sys.argv[2]:
-                for item in data["tasks"]:
-                    table.add_row(item.values())
+        task = check_id(int(sys.argv[2]), data["tasks"])
+        if task:
+            task["status"] = sys.argv[1][5:]
+            task["updatedAt"] = current_datetime
+            Json_file().dump(data, "Updated status")
+        else:
+            sys.exit("No task with this id")
 
-# uptated the status
-elif sys.argv[1] in ["mark-in-progress", "mark-done"]:
-    if len(sys.argv) == 3:
-        for item in data["tasks"]:
-            if item["id"] == int(sys.argv[2]):
-                item["status"] = sys.argv[1][5:]
-                item["updatedAt"] = datetime
-                break
-        Json_file().dump(data, "Updated status")
-
-    else:
-        # more than 3 argv
-        sys.exit(Msg.exceeded)
-
-# invalid argv
-else:
-    sys.exit(Msg.invalid)
+    # invalid argv
+    case _:
+        print("Invalid arguments. Type task-cli help to see the manual")
