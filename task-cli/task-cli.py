0d773a325ca3782041c469fe141f06109a540a3e
Maicon <maicon.rodrigues017@proton.me>
2025-05-12 15:32:28 -0300

    modified task-cli.py

--- task-cli/task-cli.py
+++ task-cli/task-cli.py
@@ -3,7 +3,7 @@ import datetime
 
 from utils.manual import Manual
 from utils.model import Task
-from utils.json import Json_file
+from utils.json_manager import Json_manager
 from utils.error_messages import valid_args, min_args, valid_status, check_id
 
 from prettytable import PrettyTable
@@ -12,7 +12,8 @@ from prettytable import PrettyTable
 current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # load json file
-data = Json_file().load()
+data = Json_manager().load()
+tasks = data["tasks"]
 
 match sys.argv[1]:
     # manual
@@ -21,79 +22,77 @@ match sys.argv[1]:
 
     # new task
     case "add":
+        # checking for invalid arguments
         min_args(3, "add")
 
         # id auto increment
-        if not data["tasks"]:
-            id = 0
-        else:
-            id = data["tasks"][-1]["id"] + 1
+        id = max(int(id["id"]) for id in tasks) + 1 if tasks else 0
 
         new_task = Task(id, sys.argv[2], "todo", current_datetime, current_datetime)
-        data["tasks"].append(new_task.dictionary())
-        Json_file().dump(data, "Added task")
+        Json_manager().add(new_task.dictionary())
 
     # updating tasks
     case "update":
+        # checking for invalid arguments
         min_args(4, "update")
 
-        task = check_id(int(sys.argv[2]), data["tasks"])
+        # checking if there is a task with that id
+        check = check_id(int(sys.argv[2]), tasks)
 
-        if task:
-            task["description"] = sys.argv[3]
-            task["updatedAt"] = current_datetime
-            Json_file().dump(data, "Updated task")
+        if check:
+            Json_manager().update(int(sys.argv[2]), sys.argv[3], current_datetime)
         else:
             sys.exit("No task with this id")
 
     # deleting tasks
     case "delete":
+        # checking for invalid arguments
         valid_args(3, "delete")
 
-        check_id = check_id(int(sys.argv[2]), data["tasks"])
+        # checking if there is a task with that id
+        check = check_id(int(sys.argv[2]), tasks)
 
-        if check_id:
-            data["tasks"] = [
-                item for item in data["tasks"] if item["id"] != int(sys.argv[2])
-            ]
-            Json_file().dump(data, "Deleted task")
+        if check:
+            Json_manager().delete(int(sys.argv[2]))
         else:
             sys.exit("No task with this id")
 
-    # listing all tasks
+    # listing tasks
     case "list":
-        if not data["tasks"]:
+        if not tasks:
             sys.exit("No tasks")
 
-        table = PrettyTable(data["tasks"][0].keys())
+        table = PrettyTable(tasks[0].keys())
 
+        # listing all tasks
         if len(sys.argv) < 3:
-            for item in data["tasks"]:
+            for item in tasks:
                 table.add_row(item.values())
 
+        # list tasks by status
         else:
-            # list tasks by status
+            # checking for invalid arguments
             valid_args(3, "list-status")
             valid_status(sys.argv[2])
-            for item in [
-                item for item in data["tasks"] if item["status"] == sys.argv[2]
-            ]:
+
+            for item in [item for item in tasks if item["status"] == sys.argv[2]]:
                 table.add_row(item.values())
 
         print(table)
 
     # uptated the status
     case "mark-in-progress" | "mark-done":
+        # checking for invalid arguments
         valid_args(3, "mark")
 
-        task = check_id(int(sys.argv[2]), data["tasks"])
-        if task:
-            task["status"] = sys.argv[1][5:]
-            task["updatedAt"] = current_datetime
-            Json_file().dump(data, "Updated status")
+        # checking if there is a task with that id
+        check = check_id(int(sys.argv[2]), tasks)
+
+        if check:
+            Json_manager().mark(sys.argv[1], int(sys.argv[2]), current_datetime)
         else:
             sys.exit("No task with this id")
 
-    # invalid argv
+    # invalid arguments
     case _:
-        print("Invalid arguments. Type task-cli help to see the manual")
+        sys.exit("Invalid arguments. Type task-cli help to see the manual")
